cd39d08aa9a9d1f85edec04f3b7c294e07884e7d
Vafa AD <vafa.andalibi@gmail.com>
2016-06-02 20:13:10 +0300

    monitor fixes, saving added

--- Plotter.py
+++ Plotter.py
@@ -1,6 +1,8 @@
 from matplotlib.pyplot import  *
+from brian2 import *
 
-def multi_y_plotter(ax, len, x,y):
+def multi_y_plotter(ax, len, variable,item,title):
+
     for i in range(len):
-        tmp_str = ax + '.plot(x, y[%d])'%i
-        exec tmp_str
+        tmp_str = 'ax.plot(item.t/ms, item.%s[%d])'%(variable,i);exec tmp_str
+        tmp_str = "ax.set_title('%s')" % (title);exec tmp_str
