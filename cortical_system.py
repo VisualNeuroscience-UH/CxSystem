8542cd1497d092210d70c2f175d8d2c3dfd97446
Vafa AD <vafa.andalibi@gmail.com>
2016-07-04 00:01:21 +0300

    Documentation Update

--- cortical_system.py
+++ cortical_system.py
@@ -44,7 +44,9 @@ class cortical_system(object):
         * neurongroups_list: This list contains name of the NeuronGroup() instances that are placed in the Globals().
         * synapses_list: This list contains name of the Synapses() instances that are placed in the Globals().
         * monitor_name_bank: The dictionary containing the name of the monitors that are defined for any NeuronGroup() or Synapses().
-        * default_monitors:
+        * default_monitors: In case --> and <-- symbols are used in the configuration file, this default monitor will be applied on all the target lines in between those marks.
+        * save_data: The save_data() object for saving the final data.
+
         '''
 
         _options = {
@@ -58,17 +60,17 @@ class cortical_system(object):
         self.neurongroups_list = [] # This list contains name of the NeuronGroup() instances that are placed in the Globals().
         self.synapses_list = [] # This list contains name of the Synapses() instances that are placed in the Globals().
         self.monitor_name_bank = {} # The dictionary containing the name of the monitors that are defined for any NeuronGroup() or Synapses().
-        self.default_monitors = []
+        self.default_monitors = [] #In case --> and <-- symbols are used in the configuration file, this default monitor will be applied on all the target lines in between those marks.
         self.monitor_idx = 0
-        self.save_data = save_data(save_path)
+        self.save_data = save_data(save_path) #The save_data() object for saving the final data.
         self.save_data.creat_key('positions_all')
         self.save_data.data['positions_all']['w_coord']= {}
         self.save_data.data['positions_all']['z_coord'] = {}
-        with open (config_path, 'r') as f :
+        with open (config_path, 'r') as f : # Here is the configuration file parser.
             for line in f:
                 if line[0]  in is_tag :
-                    line = line[line.index(']')+1:]
-                    tag = line[line.index('['):line.index(']')+1]
+                    line = line[line.index(']')+1:] # Trip the index number
+                    tag = line[line.index('['):line.index(']')+1] # extract the main tag
                     assert tag in _options.keys(), 'The tag %s is not defined.'%tag
                 else:
                     continue
@@ -85,23 +87,34 @@ class cortical_system(object):
         '''
         The method that creates the NeuronGroups() based on the parameters that are extracted from the configuraiton file in the __init__ method of the class.
 
-        :param args:
-        :return:
+        :param args: This method will have at least 3 main positional argumenst: Number of Neurons in the group, Type of neuron in the group and layer index (description of the layer index can be found in the configuration file.
+
+        Main internal variables:
+
+        * mon_args: contains the monitor arguments extracted from the target line.
+        * net_center: center position of the neuron group in visual field coordinates, description can be found in configuration file tutorial.
+        * NG_name: Generated vriable name for the NeuonGroup().
+        * NN_name: Generated vriable name for corresponding Neuron Number.
+        * NE_name: Generated vriable name for the NeuonGroup() equation.
+        * NT_name: Generated vriable name for the NeuonGroup() threshold.
+        * NRes_name: Generated vriable name for the NeuonGroup() reset value.
+        * NRef_name: Generated vriable name for the NeuonGroup() refractory value.
+        * NNS_name: Generated vriable name for the NeuonGroup() namespace.
+        * NG_init: NeuronGroups() should be initialized with a random vm, ge and gi values. To address this, a 6-line code is generated and put in this variable, the running of which will lead to initialization of current NeuronGroup().
         '''
         args = args[0]
-        mon_args=[]
-        net_center = 0+0j
-        #check monitors in line:
-        if '[M]' in args :
+        mon_args=[] # contains the monitor arguments extracted from the target line.
+        net_center = 0+0j # center position of the neuron group in visual field coordinates, description can be found in configuration file tutorial.
+        if '[M]' in args : # check if target line contains monitors
             mon_args = args[args.index('[M]')+1:]
             args = args[0:args.index('[M]')]
-        if '[CN]' in args:
+        if '[CN]' in args: # check if target line contains center position.
             net_center= complex(args[args.index('[CN]')+1])
             args = args[0:args.index('[CN]')]
         current_idx=  len(self.customized_neurons_list)
-        if args[1] == 'PC' :
+        if args[1] == 'PC' : # extract the layer index of PC neurons separately (since it is in form of a list like [4,1]
             exec 'args[2] = array(' + args[2] + ')'
-        self.customized_neurons_list.append(customized_neuron (*args[0:3],network_center = net_center).output_neuron) # layer_idx is created by dynamic compiler
+        self.customized_neurons_list.append(customized_neuron (*args[0:3],network_center = net_center).output_neuron) # creating a customized_neurons_list object and feeding the positional arguments to it. The main member of the class called output_neuron is then appended to customized_neurons_list.
         if len(args) > 3: # in case of threshold/reset/refractory overwrite
             for arg_idx in range (3,len(args)):
                 if 'threshold' in args[arg_idx] :
@@ -113,31 +126,36 @@ class cortical_system(object):
                 elif 'refractory' in args[arg_idx]:
                     args[arg_idx] = args[arg_idx][args[arg_idx].index("'")+1:-2]
                     self.customized_neurons_list[-1]['refractory'] =  args[arg_idx]
-        NG_name = self._NeuronGroup_prefix + str(current_idx) + '_' + args[1]
+        NG_name = self._NeuronGroup_prefix + str(current_idx) + '_' + args[1] # Generated vriable name for the NeuonGroup().
         self.neurongroups_list.append(NG_name)
-        NN_name = self._NeuronNumber_prefix + str(current_idx)
-        NE_name = self._NeuronEquation_prefix + str(current_idx)
-        NT_name = self._NeuronThreshold_prefix + str(current_idx)
-        NRes_name = self._NeuronReset_prefix + str(current_idx)
-        NRef_name = self._NeuronRef_prefix + str(current_idx)
-        NNS_name = self._NeuronNS_prefix + str(current_idx)
+        NN_name = self._NeuronNumber_prefix + str(current_idx) # Generated vriable name for corresponding Neuron Number.
+        NE_name = self._NeuronEquation_prefix + str(current_idx) # Generated vriable name for the NeuonGroup() equation.
+        NT_name = self._NeuronThreshold_prefix + str(current_idx) # Generated vriable name for the NeuonGroup() threshold.
+        NRes_name = self._NeuronReset_prefix + str(current_idx) # Generated vriable name for the NeuonGroup() reset value.
+        NRef_name = self._NeuronRef_prefix + str(current_idx) # Generated vriable name for the NeuonGroup() refractory value.
+        NNS_name = self._NeuronNS_prefix + str(current_idx) # Generated vriable name for the NeuonGroup() namespace.
         # NPos_name = self._NeuronPos_prefix + str(current_idx)
         # self.monitor_name_bank['NeuroGroups'].append(NG_name)
 
+        # next  6 line create the variable that are needed for current target line NeuronGroup().
         exec "%s=self.customized_neurons_list[%d]['number_of_neurons']"% (NN_name, current_idx)
         exec "%s=self.customized_neurons_list[%d]['equation']"% (NE_name , current_idx)
         exec "%s=self.customized_neurons_list[%d]['threshold']"%(NT_name ,current_idx)
         exec "%s=self.customized_neurons_list[%d]['reset']"% (NRes_name, current_idx)
         exec "%s=self.customized_neurons_list[%d]['refractory']"% (NRef_name,current_idx)
         exec "%s=self.customized_neurons_list[%d]['namespace']"% (NNS_name,current_idx)
+        # Creating the actual NeuronGroup() using the variables in the previous 6 lines
         exec "%s= NeuronGroup(%s, model=%s, threshold=%s, reset=%s,refractory = %s, namespace = %s)" \
                  % (NG_name , NN_name , NE_name,NT_name, NRes_name, NRef_name , NNS_name)
+        # Setting the position of the neurons in the current Neurong Group.
         exec "%s.x=real(self.customized_neurons_list[%d]['w_positions'])*mm\n%s.y=imag(self.customized_neurons_list[%d]['w_positions'])*mm" % (
         NG_name, current_idx, NG_name, current_idx)
 
+        # Saving the neurons' positions both in visual field and cortical coordinates in save_data() object.
         self.save_data.data['positions_all']['z_coord'][NG_name] = self.customized_neurons_list[current_idx]['z_positions']
         self.save_data.data['positions_all']['w_coord'][NG_name] = self.customized_neurons_list[current_idx]['w_positions']
 
+        # NeuronGroups() should be initialized with a random vm, ge and gi values. To address this, a 6-line code is generated and put in NG_init variable, the running of which will lead to initialization of current NeuronGroup().
         NG_init = 'Vr_offset = rand(len(%s))\n'%NG_name
         NG_init += "for _key in %s.variables.keys():\n"%NG_name
         NG_init += "\tif _key.find('vm')>=0:\n"
@@ -145,26 +163,35 @@ class cortical_system(object):
         NG_init += "\telif ((_key.find('ge')>=0) or (_key.find('gi')>=0)):\n"
         NG_init += "\t\tsetattr(%s,_key,0)" %NG_name
         exec NG_init
+        # updating the Globals()
         exec "globals().update({'%s':%s,'%s':%s,'%s':%s,'%s':%s,'%s':%s,'%s':%s,'%s':%s})" % \
              (NN_name, NN_name, NE_name, NE_name, NT_name, NT_name, NRes_name, NRes_name, NRef_name, NRef_name, NNS_name,
              NNS_name, NG_name, NG_name)
-        self.monitors(mon_args,args[1],NG_name , self.customized_neurons_list[-1]['equation'])
+        # passing remainder of the arguments to monitors() method to take care of the arguments.
+        self.monitors(mon_args,NG_name , self.customized_neurons_list[-1]['equation'])
 
 
-    def monitors(self,mon_args,cell_type, object_name , equation):
+    def monitors(self,mon_args, object_name , equation):
         '''
-        The method that creates the monitors() based on the parameters that are extracted from the configuraiton file in the __init__ method of the class.
+        This method creates the Monitors() in brian2 based on the parameters that are extracted from a target line in configuraiton file.
 
-        :param mon_args:
-        :param cell_type:
-        :param object_name:
-        :param equation:
-        :return:
+        :param mon_args: The monitor arguments extracted from the target line.
+        :param object_name: The generated name of the current object.
+        :param equation: The equation of the NeuronGroup() to check if the target variable exist in it.
+
+        Main internal variables:
+
+        * mon_tag: The tag that is extracted from the target line everytime.
+        * Mon_name: Generated variable name for a specific monitor.
+        * Mon_str: The syntax used for building a specific StateMonitor.
+        * sub_mon_tags: The tags in configuration file that are specified for a StateMonitor(), e.g. in record=True which is specified by [rec]True in configuration file, [rec] is saved in sub_mon_tags
+        * sub_mon_args: The corresponding arguments of sub_mon_tags for a StateMonitor(), e.g. in record=True which is specified by [rec]True in configuration file, True is saved in sub_mon_args.
         '''
         if not mon_args and not self.default_monitors:
             return
         if not mon_args:
             mon_args = self.default_monitors
+        # check if default monitor should be applied or not
         if '-->' in mon_args:
             del (mon_args[mon_args.index('-->')])
             self.default_monitors = mon_args
@@ -182,47 +209,52 @@ class cortical_system(object):
         }
         self.monitor_name_bank[object_name] = []
         for mon_arg in mon_args:
+            # Extracting the monitor tag
             mon_tag = mon_arg[mon_arg.index('['):mon_arg.index(']') + 1]
             mon_arg = mon_arg.replace(mon_tag, '')
-            if mon_tag == '[Sp]':
+            if mon_tag == '[Sp]': # This is for SpikeMonitor()s
+                # If there is a spike monitor, a spikes_all field is added to the save_data() object.
                 self.save_data.creat_key('spikes_all')
-                Mon_name = monitor_options['[Sp]'][0] + str(self.monitor_idx) + '_' + object_name
-                self.save_data.syntax_bank.append("self.save_data.data['spikes_all']['%s'] = %s.it"%(object_name,Mon_name))
+                Mon_name = monitor_options['[Sp]'][0] + str(self.monitor_idx) + '_' + object_name # Generated variable name for a specific monitor.
+                self.save_data.syntax_bank.append("self.save_data.data['spikes_all']['%s'] = %s.it"%(object_name,Mon_name)) # After simulation, this syntax will be used to save this specific monitor's result.
                 self.monitor_name_bank[object_name].append(Mon_name)
+                # build the monitor object based on the generated name:
                 exec  "%s=%s(%s)" % (Mon_name, monitor_options['[Sp]'][1], object_name)
+                # update the Globals():
                 exec "globals().update({'%s':%s})" %(Mon_name, Mon_name)
+                # update monitor index:
                 self.monitor_idx +=1
-            else:
+            else: # Work on StateMonitors() :
+                # Split the StateMonitors if there are multiple of them separated by a "+" :
                 mon_arg = mon_arg.split('+')
-                for sub_mon_arg in mon_arg:
-                    # Mon_name = monitor_options['[St]'][0] + str(self.monitor_idx) + '_' + object_name + '_' +
-                    Mon_str = "=%s(%s,"% ( str(monitor_options[mon_tag][1]),object_name)
-                    sub_mon_tags = []
-                    if not ('[' in sub_mon_arg):
+                for sub_mon_arg in mon_arg: # going through each state variable:
+                    Mon_str = "=%s(%s,"% ( str(monitor_options[mon_tag][1]),object_name) # The syntax used for building a specific StateMonitor.
+                    sub_mon_tags = [] # The tags in configuration file that are specified for a StateMonitor(), e.g. in record=True which is specified by [rec]True in configuration file, [rec] is saved in sub_mon_tags
+                    if not ('[' in sub_mon_arg): # if there is no tag, it means that the only tag that should be there is record = true
                         sub_mon_arg = sub_mon_arg.split()
                         sub_mon_arg.append('True')
                         sub_mon_tags.append('[rec]')
                         # Mon_name = monitor_options['[St]'][0] + str(self.monitor_idx) + '_' + object_name + '_' + \
                         #        sub_mon_arg
                     else:
-                        tag_open_indices = [idx for idx, ltr in enumerate(sub_mon_arg) if ltr == '[']
-                        tag_close_indices = [idx for idx, ltr in enumerate(sub_mon_arg) if ltr == ']']
+                        tag_open_indices = [idx for idx, ltr in enumerate(sub_mon_arg) if ltr == '['] # find the start index of all tags
+                        tag_close_indices = [idx for idx, ltr in enumerate(sub_mon_arg) if ltr == ']'] # find the end index of all tags
                         assert len(tag_open_indices)==len(tag_close_indices), 'Error: wrong sets of tagging paranteses in monitor definitoins. '
-                        for tag_idx in range(len(tag_open_indices)) :
+                        for tag_idx in range(len(tag_open_indices)) : # go through each StateMonitor tag:
                             sub_mon_tags.append(sub_mon_arg[sub_mon_arg.index('['):sub_mon_arg.index(']') + 1])
-                            sub_mon_arg = sub_mon_arg.replace(sub_mon_tags[tag_idx],' ') # replace the tags with space
+                            sub_mon_arg = sub_mon_arg.replace(sub_mon_tags[tag_idx],' ')
                         sub_mon_arg = sub_mon_arg.split(' ')
-                        if not '[rec]' in sub_mon_tags  :
+                        if not '[rec]' in sub_mon_tags  : # if some tags exist and [rec] is not present, it means record=True
                             sub_mon_tags.append('[rec]')
                             sub_mon_arg.append('True')
                         assert len(sub_mon_arg) == len(sub_mon_tags) + 1 , 'Error in monitor tag definition.'
-                    self.save_data.creat_key('%s_all'%sub_mon_arg[0])
+                    self.save_data.creat_key('%s_all'%sub_mon_arg[0]) # Create a key in save_data() object for that specific StateMonitor variable.
                     Mon_name = monitor_options['[St]'][0] + str(self.monitor_idx) + '_' + object_name + '__' + sub_mon_arg[0]
-                    self.save_data.syntax_bank.append("self.save_data.data['%s_all']['%s'] = asarray(%s.%s)" % (sub_mon_arg[0], object_name, Mon_name,sub_mon_arg[0]))
+                    self.save_data.syntax_bank.append("self.save_data.data['%s_all']['%s'] = asarray(%s.%s)" % (sub_mon_arg[0], object_name, Mon_name,sub_mon_arg[0])) # After simulation, this syntax will be used to save this specific monitor's result.
 
                     self.monitor_name_bank[object_name].append(Mon_name)
                     Mon_str = Mon_name + Mon_str + "'" + sub_mon_arg[0] + "'"
-                        # check if the variable exist in the equation
+                        # check if the variable exist in the equation:
                     if ('d' + sub_mon_arg[0]) in str(equation):
                         assert (sub_mon_arg[0] + '/') in str(equation), \
                             'The monitor varibale %s is not defined in the equation.' % sub_mon_arg[0]
@@ -230,10 +262,13 @@ class cortical_system(object):
                         assert (sub_mon_arg[0]) in str(equation), \
                             'The monitor varibale %s is not defined in the equation.' % sub_mon_arg[0]
                     del(sub_mon_arg[0])
+                    # add each of the tag and their argument, e.g. "record" as tag and "True" as argument, to the Mon_str syntax string.
                     for idx,tag in enumerate(sub_mon_tags):
                         Mon_str += monitor_options[tag][0] + sub_mon_arg[idx]
                     Mon_str +=')'
+                    # create the Monitor() object
                     exec Mon_str
+                    # Update the Globals()
                     exec "globals().update({'%s':%s})" % (Mon_name, Mon_name)
                     self.monitor_idx += 1
 
@@ -243,8 +278,11 @@ class cortical_system(object):
         '''
         The method that creates the Synapses() in brian2, based on the parameters that are extracted from the configuraiton file in the __init__ method of the class.
 
-        :param args:
-        :return:
+        :param args: This method will have at least 4 main positional argumenst: The transmitter, e.g. ge or gi, presynaptic neurong group index, post synaptic group index, and type of Synaptic connection , i.e. STDP or Fixed.
+
+        Main internal variables:
+
+        * mon_args: contains the monitor arguments extracted from the target line.
         '''
         _options = {
             '[C]': self.neuron_group,
@@ -346,7 +384,7 @@ class cortical_system(object):
             except:
                 exec "globals().update({'%s':%s,'%s':%s,'%s':%s,'%s':%s})" % \
                      (SE_name, SE_name, SPre_name, SPre_name,  SNS_name, SNS_name, S_name, S_name)
-            self.monitors(mon_args, syn[3], S_name, self.customized_synapses_list[-1]['equation'])
+            self.monitors(mon_args, S_name, self.customized_synapses_list[-1]['equation'])
 
     def relay(self,*args):
         '''
