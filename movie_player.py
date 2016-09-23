b6b3ab77fcfdae0e88afc9e43625c06a582ef74c
Vafa AD <vafa.andalibi@gmail.com>
2016-09-23 21:52:40 +0300

    Movie player added

--- movie_player.py
+++ movie_player.py
@@ -2,59 +2,125 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.lines import Line2D
 import matplotlib.animation as animation
+from matplotlib.animation import FuncAnimation
+import scipy.io
+from numpy import *
+import pylab as pl
+from matplotlib import colors
+from sympy.ntheory.factor_ import smoothness
 
 
-# This example uses subclassing, but there is no reason that the proper
-# function couldn't be set up and then use FuncAnimation. The code is long, but
-# not really complex. The length is due solely to the fact that there are a
-# total of 9 lines that need to be changed for the animation as well as 3
-# subplots that need initial set up.
-class SubplotAnimation(animation.TimedAnimation):
-    def __init__(self):
-        fig = plt.figure()
-        ax1 = fig.add_subplot(1, 2, 1)
+def spike_to_fram(groups,spikes,X_axis,Y_axis,z_coord,axis_precision,smoothness=15,d_t = 0.001) :
+    if smoothness%2!=1 :
+        smoothness+=1
+    ts = []
+    for group in groups:
+        spikes_ts_tmp = spikes[group][0][0][1]
+        ts_temp = [round(t,int(abs(log10(d_t)))) for t in arange(min(spikes_ts_tmp), max(spikes_ts_tmp)+d_t, d_t)]
+        ts = ts_temp if len(ts)<len(ts_temp) else ts
+    number_of_frames = len(ts)
+    all_frames = zeros([number_of_frames*smoothness,size(X_axis),size(Y_axis)])
+    number_of_pixel_colors = smoothness/2+1
+    # step = 1./number_of_pixel_colors
+    colors_ = [1-1./xx for xx in range(1,number_of_pixel_colors+1)]
+    colors_ = list(colors_) + list(colors_[::-1][1:])
+    for idx in range(number_of_frames):
+        for group in groups:
+            spikes_tmp = [round(t,int(abs(log10(d_t)))) for t in spikes[group][0][0][1]]
+            if ts[idx] in spikes_tmp:
+                # print  "%f found" %ts[idx]
+                indices =[ii for ii,xx in enumerate(spikes_tmp) if xx==ts[idx]]
+                action_potentials = [int(action_idx) for action_idx in spikes[group][0][0][0][indices]]
+                for action in action_potentials:
+                    x_target = round(real(z_coord[group][0][0][0][action]),axis_precision)
+                    x_target_index = X_axis.index(x_target)
+                    y_target = round(imag(z_coord[group][0][0][0][action]), axis_precision)
+                    y_target_index = Y_axis.index(y_target)
+                    for smoo in range(smoothness):
+                        color_value =  colors_ [smoo] * (groups.index(group)+1)
+                        print '%f' %(color_value)
+                        all_frames[idx*smoothness+smoo][x_target_index-1:x_target_index+1,y_target_index-1:y_target_index+1] = color_value
+    return all_frames
 
-        self.t = np.linspace(0, 80, 400)
-        self.x = np.cos(2 * np.pi * self.t / 10.)
-        self.y = np.sin(2 * np.pi * self.t / 10.)
-        self.z = 10 * self.t
 
-        ax1.set_xlabel('x')
-        ax1.set_ylabel('y')
-        self.line1 = Line2D([], [], color='black')
-        self.line1a = Line2D([], [], color='red', linewidth=2)
-        self.line1e = Line2D(
-            [], [], color='red', marker='o', markeredgecolor='r')
-        ax1.add_line(self.line1)
-        ax1.add_line(self.line1a)
-        ax1.add_line(self.line1e)
-        ax1.set_xlim(-2, 2)
-        ax1.set_ylim(-2, 2)
-        ax1.set_aspect('equal', 'datalim')
+data = scipy.io.loadmat('/home/corriel/Documents/Git_repos/CX_Output/Custom_Name_20160916_165114.mat')
+positions = data['positions_all'][0]
+spikes = data['spikes_all']
+z_coord = positions['z_coord'][0]
+dtypes = z_coord.dtype
+groups = [l for l in dtypes.fields]
+min_x = 0
+max_x = 0
+min_y = 0
+max_y = 0
+for group in groups:
+    min_x = min_x if min_x<min(real(z_coord[group][0][0][0])) else min(real(z_coord[group][0][0][0]))
+    max_x = max_x if max_x>max(real(z_coord[group][0][0][0])) else max(real(z_coord[group][0][0][0]))
+    min_y = min_y if min_y < min(imag(z_coord[group][0][0][0])) else min(imag(z_coord[group][0][0][0]))
+    max_y = max_y if max_y > max(imag(z_coord[group][0][0][0])) else max(imag(z_coord[group][0][0][0]))
+d_x = 1e-4 # equal to 100um
+min_x = round(min_x, int(abs(log10(d_x))))
+max_x = round(max_x, int(abs(log10(d_x))))
+min_y = round(min_y, int(abs(log10(d_x))))
+max_y = round(max_y, int(abs(log10(d_x))))
+axis_precision = int(abs(log10(d_x)))
 
 
-        animation.TimedAnimation.__init__(self, fig, interval=50, blit=True)
+X_axis = [round(cord, axis_precision) for cord in arange(min_x, max_x+d_x, d_x)]
+Y_axis = [round(cord, axis_precision) for cord in arange(min_y, max_y+d_x, d_x)]
 
-    def _draw_frame(self, framedata):
-        i = framedata
-        head = i - 1
-        head_len = 10
-        head_slice = (self.t > self.t[i] - 1.0) & (self.t < self.t[i])
+test_zs = z_coord['NG1_SS_L4'][0][0][0]
 
-        self.line1.set_data(self.x[:i], self.y[:i])
-        self.line1a.set_data(self.x[head_slice], self.y[head_slice])
-        self.line1e.set_data(self.x[head], self.y[head])
+smoothness = 15
+all_frames = spike_to_fram(groups, spikes, X_axis, Y_axis, z_coord, axis_precision,smoothness=smoothness)
+fig, ax = plt.subplots(1, 1)
+# cmap = colors.ListedColormap(['white', 'red' , 'blue' , 'black'])
+# bounds=[-0.5,0.5,1.5,2.5,3.5]
+# norm = colors.BoundaryNorm(bounds, cmap.N)
 
-        self._drawn_artists = [self.line1, self.line1a, self.line1e]
 
-    def new_frame_seq(self):
-        return iter(range(self.t.size))
 
-    def _init_draw(self):
-        lines = [self.line1, self.line1a, self.line1e]
-        for l in lines:
-            l.set_data([], [])
 
-ani = SubplotAnimation()
-#ani.save('test_sub.mp4')
+# im = ax.imshow(all_frames[0], origin='lower',
+#                extent=(min_x-min_x/3, max_x+max_x/3, min_y-min_y/3, max_y+max_y/3),
+#                vmin=0, vmax=groups.__len__(),  cmap=cmap, norm=norm)
+
+im = ax.imshow(all_frames[0], origin='lower',
+               extent=(min_x-min_x/3, max_x+max_x/3, min_y-min_y/3, max_y+max_y/3),
+               vmin=0, vmax=groups.__len__(),cmap = "spectral")
+
+
+class animator:
+    def __init__(self,fig,data,im):
+        self.fig = fig
+        self.data = data
+        self.i = 0
+        self.im = im
+    def animator (self,index):
+        self.im.set_data(self.data[index])
+        self._status_printer("frame %d"%(index/smoothness))
+    def _status_printer(self,str):
+        cleaner = ' ' * 100
+        print '\r' + cleaner + '\r' + str,
+# mycmap = pl.cm.jet # for example
+# for entry in pl.unique(all_frames)[1:]:
+#     mycolor = mycmap(entry*255/(max(asarray(all_frames).reshape(-1)) - min(asarray(all_frames).reshape(-1))))
+#     pl.plot(0, 0, "-", c=mycolor, label='hello')
+#
+# pl.imshow(all_frames[0])
+# pl.legend()
+
+
+
+anime = animator (fig,all_frames,im)
+
+anim = FuncAnimation(fig, anime.animator, frames=all_frames.shape[0],interval=400/smoothness)
+
+
 plt.show()
+
+
+
+
+
+
