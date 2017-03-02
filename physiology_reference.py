183a98c8e8da11d5d9c48cba3d0680f59c8ecbbd
sivanni <sivanni@users.noreply.github.com>
2017-03-02 06:28:56 +0200

    Copyright update

--- physiology_reference.py
+++ physiology_reference.py
@@ -1,7 +1,9 @@
 
 '''
-The program is distributed under the terms of the GNU General Public License
-Copyright 2017 Vafa Andalibi, Simo Vanni, Henri Hokkanen.
+The preliminary version of this software has been developed at Aalto University 2012-2015, 
+and the full version at the University of Helsinki 2013-2017. The software is distributed 
+under the terms of the GNU General Public License. 
+Copyright 2017 Vafa Andalibi, Henri Hokkanen and Simo Vanni.
 '''
 
 from networkx.drawing import layout
